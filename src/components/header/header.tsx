30dc2666fa38c3a7d8a53c3554b62dc6d0c58138
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-10-06 23:54:18 +0600

    working on the toggle

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../../context/theme-provider";
 
 const Header = () => {
+    const {theme} = useTheme();
     return (
-        <header>
-            <div>
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+            <div className="container mx-auto flex h-16 items-center justify-between px-4">
                 <Link to={'/'}>
-                <img src="solara.png" alt="Solara" className="h-25" />
+                <img src="solara3.png" alt="Solara" className="h-25" />
                 </Link>
+            </div>
+            <div>
+                {/* <search>
+                </search> */}
+                {/* theme toggle */}
             </div>
         </header>
     );
