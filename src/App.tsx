f30439529091ea050365b8440a1210afb4dcd27c
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-10-07 23:11:47 +0600

    set up the tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,27 @@ import Layout from './Layout/Layout'
 import { ThemeProvider } from './context/theme-provider'
 import WeatherDashboard from './pages/weather_dashboard'
 import CityPage from './pages/city_page'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <Layout>
-          <Routes>
-            <Route path='/' element={<WeatherDashboard />} />
-            <Route path='/city/:cityName' element={<CityPage />} />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<WeatherDashboard />} />
+              <Route path='/city/:cityName' element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
